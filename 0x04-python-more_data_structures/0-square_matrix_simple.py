73aec5b6e1384ce16f935e28cd34ad6b1428b77e
mahi7000 <mahlet7000@gmail.com>
2024-01-10 09:40:57 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for i in range(0 ,len(matrix)):
-        new_matrix.append(list(map(lambda x: x **2, matrix[i])))
+    for i in range(0, len(matrix)):
+        new_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
     return (new_matrix)

1256dfe09a5b6e1c1ef5a9f47300ccd1095eaf4f
mahi7000 <mahlet7000@gmail.com>
2024-02-28 08:03:44 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" class with instantiation optional and more complicated """
+
+
+class Square:
+    """ inputing size is optional size is checked """
+    def __init__(self, size=0):
+        self.size = size
+
+    """ size getter """
+    @property
+    def size(self):
+        return self.__size
+
+    """ size setter """
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """ finds the area of the square """
+    def area(self):
+        return (self.__size ** 2)

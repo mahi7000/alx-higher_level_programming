e3b2032c7d0c9be7e51ce4552fdaa4eac40c9ab5
mahi7000 <mahlet7000@gmail.com>
2024-02-28 04:32:21 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class with instantiation optional and more complicated """
+
+
+class Square:
+    """ inputing size is optional size is checked """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """ finds the area of the square """
+    def area(self):
+        return (self.__size ** 2)

b346d8e33a43805b686d849e9db20e5c9f6e979b
mahi7000 <mahlet7000@gmail.com>
2024-02-28 04:11:03 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# python3 -c 'print(__import__("my_module").__doc__)'
+""" an empty class Square that defines a square """
 
 class Square:
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """ an empty square class """
     pass

6609bef5bebab7d533850799445352cbb6832918
mahi7000 <mahlet7000@gmail.com>
2024-03-04 22:41:47 -0800

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" adds 2 integers """
+
+def add_integer(a, b=98):
+    """ Add function """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b

73672084f351c744872da298054862c1e42195a0
mahi7000 <mahlet7000@gmail.com>
2024-03-07 00:06:06 -0800

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" prints a text with 2 new lines after each of these characters: ., ? and : """
+
+
+def text_indentation(text):
+    """ prints a text with 2 new lines after  ., ? and : """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    flag = 0
+    for letter in text:
+        if flag == 0:
+            if letter == ' ':
+                continue
+            else:
+                flag = 1
+
+        if flag == 1:
+            if letter == '.' or letter == ':' or letter == '?':
+                print(letter + '\n')
+                flag = 0
+            else:
+                print(letter, end='')

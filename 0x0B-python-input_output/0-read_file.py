e1d3521e0de3a8b5fda163f33db38d82d5e2c8f0
mahi7000 <mahlet7000@gmail.com>
2024-03-11 04:46:54 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ reads file """
     with open(filename, 'r', encoding='utf8') as file:
-        text = file.read()
-    print(text)
+        for line in file:
+            print(line, end='')

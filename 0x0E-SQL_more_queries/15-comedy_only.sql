960fd6b7a1f688c8f7819b0345fdfdab19ab8ec1
mahi7000 <mahlet7000@gmail.com>
2024-03-23 10:48:22 -0700

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,10 +2,10 @@
 
 SELECT s.title
 FROM tv_shows AS s
-	INNER JOIN tv_show_genres as t
+	INNER JOIN tv_show_genres AS t
 	ON t.show_id = s.id
 
-	INNER JOIN tv_genres as g
+	INNER JOIN tv_genres AS g
 	ON g.id = t.genre_id
 	WHERE g.name = "Comedy"
 ORDER BY s.title;

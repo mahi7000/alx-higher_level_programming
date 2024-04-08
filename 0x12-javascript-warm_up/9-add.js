8c8873930f94f4400ae6192b82b232bacf1a15ed
mahi7000 <mahlet7000@gmail.com>
2024-04-08 10:43:23 -0700

    Add arguments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-function add(a, b) {
-	return (a + b);
+function add (a, b) {
+  return (a + b);
 }
 
-let result = add(parseInt(argv[2]), parseInt(argv[3]));
+const result = add(parseInt(argv[2]), parseInt(argv[3]));
 console.log(result);

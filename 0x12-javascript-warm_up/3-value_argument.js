f4b1cebf48531d3ced9b2c12cb1a18a1cf66ee46
mahi7000 <mahlet7000@gmail.com>
2024-04-08 07:53:28 -0700

    Print fist argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+let count = 0;
+argv.forEach((val, index) => {
+  count = count + 1;
+});
+
+if (count === 2) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}

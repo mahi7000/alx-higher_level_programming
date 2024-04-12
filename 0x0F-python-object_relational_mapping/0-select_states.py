b568648f2a6ef414df38df84503cb561f6ca6706
mahi7000 <mahlet7000@gmail.com>
2024-04-12 01:22:13 -0700

    Select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """ lists all states from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
     username = sys.argv[1]

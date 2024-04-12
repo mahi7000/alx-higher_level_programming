c49cd6de97f0d8bb83409dbe7f16cba7a3e2986c
mahi7000 <mahlet7000@gmail.com>
2024-04-12 02:04:28 -0700

    Select cities from states

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" lists all cities of a state """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    cursor = conn.cursor()
+
+    query = """
+    SELECT cities.name
+    FROM states
+    RIGHT JOIN cities ON states.id = cities.state_id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+
+    cursor.execute(query, (search,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(", ".join([row[0] for row in rows]))
+
+    cursor.close()
+    conn.close()

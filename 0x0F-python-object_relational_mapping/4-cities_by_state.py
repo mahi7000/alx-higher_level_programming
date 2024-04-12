7980114d9975c77bc62278a5cafd92471218a8d3
mahi7000 <mahlet7000@gmail.com>
2024-04-12 01:43:45 -0700

    Select city

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     cursor = conn.cursor()
 
     query = """
-    SELECT cities.id, cities,name, states.name
+    SELECT cities.id, cities.name, states.name
     FROM cities
-    INNER JOIN states ON cities.state_id = states.id
+    LEFT JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC
     """
 

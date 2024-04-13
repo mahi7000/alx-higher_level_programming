fe0d156422a1879881ba013c87d40bd340d4bdaf
mahi7000 <mahlet7000@gmail.com>
2024-04-13 01:43:52 -0700

    Add Louisiana

---
+++ 0x0F-python-object_relational_mapping/delete.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Add Louisiana """
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = session.query(State).filter(State.id == 7).first()
+
+    session.delete(new_state)
+
+    session.commit()
+    print("suces")
+    session.close()

a41aec50773e7033b6b3eec31714e84134a7cb28
mahi7000 <mahlet7000@gmail.com>
2024-04-26 02:38:59 -0700

    Add exception

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,12 +3,14 @@
 from urllib import request, error
 import sys
 
-url = sys.argv[-1]
 
-try:
-    with request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+if __name__ == "__main__":
+    url = sys.argv[-1]
 
-except error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+    try:
+        with request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))

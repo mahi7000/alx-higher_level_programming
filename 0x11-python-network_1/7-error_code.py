f5ee94d57627705403f5ccdd9a8e79ca1dc4683f
mahi7000 <mahlet7000@gmail.com>
2024-04-26 02:54:26 -0700

    Add errors

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,12 +3,14 @@
 import requests
 import sys
 
-url = sys.argv[-1]
 
-response = requests.get(url)
-body = response.text
+if __name__ == "__main__":
+    url = sys.argv[-1]
 
-if response.status_code >= 400:
-    print("Error code: {}".format(response.status_code))
-else:
-    print(body)
+    response = requests.get(url)
+    body = response.text
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(body)

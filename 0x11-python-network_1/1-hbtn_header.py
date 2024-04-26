f6b87e556ef3fb11dbc22b5daae234267a9cb62f
mahi7000 <mahlet7000@gmail.com>
2024-04-26 02:25:34 -0700

    Meet requirements

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,6 @@ import sys
 url = sys.argv[-1]
 
 with urllib.request.urlopen(url) as response:
-    request_id = response.getheader('X-Request-Id')
+    request_id = info()['X-Request-Id']
 
     print(request_id)

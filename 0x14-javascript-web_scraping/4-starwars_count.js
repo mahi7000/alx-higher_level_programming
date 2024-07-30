5f0704f6528435ea37b89ca6256ba4f24368fdb1
Mahi <mahlet7000@gmail.com>
2024-07-30 01:39:19 -0700

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+//prints the number of movies where Wedge Antilles is present
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request.get(url, (err, response, body) => {
+	if (err) {
+		console.error(err);
+	} else {
+		const content = JSON.parse(body);
+		content.results.forEach((film) => {
+			film.characters.forEach((character) => {
+				if (character.includes(18)) {
+					count += 1;
+				}
+			});
+		});
+		console.log(count);
+	}
+});

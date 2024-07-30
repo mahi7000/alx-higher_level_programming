d28a869dcd1ab592e744e38a13abc6fa9b39115f
Mahi <mahlet7000@gmail.com>
2024-07-30 01:13:50 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//Writes a string to a file
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf-8', (err) => {
+	if (err) {
+		console.error(err)
+	}
+});

9f9c7fe9f05ca9a70edab0b1a8d2c658441ffbde
Mahi <mahlet7000@gmail.com>
2024-07-30 01:27:26 -0700

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+//print the title of Star Wars movie
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, response, body) => {
+	if (err) {
+		console.error(err);
+	} else {
+		const title = JSON.parse(body);
+		console.log(`${title.title}`);
+	}
+});
